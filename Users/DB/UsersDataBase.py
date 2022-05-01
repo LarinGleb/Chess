b9b5b76893d03cfd16eeaadd5335d9aa68944191
VforVindetta <88457706+VforVindetta@users.noreply.github.com>
2022-05-01 20:36:24 +0300

    Refactored for new data

--- Users/DB/UsersDataBase.py
+++ Users/DB/UsersDataBase.py
@@ -10,11 +10,16 @@ def AddUser(Ip: str="0.0.0.0.0", User: str = "DefaultUser", Password: str="Defau
     cursor.execute(f"""INSERT INTO Security VALUES ("{User}", "{Password}")""")
     connection.commit()
 
+def GetNameByIp(Ip: str="0.0.0.0"):
+    return cursor.execute(f"""SELECT Name FROM Users WHERE Ip = "{Ip}" """).fetchall()[0][0]
+
 def UserDefined(Ip: str="0.0.0.0.0", User: str = "DefaultUser"):
     global cursor
     Ips = cursor.execute("SELECT Ip from Users").fetchall()
     Users = cursor.execute("SELECT name from Users").fetchall()
-    print(Ips)
+
+    Users = [i[0] for i in Users]
+    Ips = [i[0] for i in Ips]
     return Ip in Ips or User in Users
 
 def PasswordAccepted(User: str="DefaultName", Password:str="DefaultPassword"):
