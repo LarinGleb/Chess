cadb996ddede9be00e1e5eb69f922fcba939f687
VforVindetta <88457706+VforVindetta@users.noreply.github.com>
2022-05-01 16:15:16 +0300

    mail sending

---
+++ Users/Mails/SendMail.py
@@ -0,0 +1,21 @@
+
+from audioop import add
+import smtplib                
+from email.mime.text import MIMEText                      
+from email.mime.multipart import MIMEMultipart
+PORT = 587
+SERVER = "smtp.gmail.com"
+
+USER, PASSWORD = open("Users/Mails/Setting.txt", "r").read().split("\n")
+def SendMail(Addr: str, code: str):
+    msg = MIMEMultipart()                                  
+    msg['From']    = USER                     
+    msg['To']      = Addr        
+    msg['Subject'] = "Protocol"   
+    body = f"Your code: {code}"
+    msg.attach(MIMEText(body, 'plain'))         
+    server = smtplib.SMTP(SERVER, PORT)       
+    server.starttls()                          
+    server.login(USER, PASSWORD)    
+    server.send_message(msg)
+    server.quit()  
